771856a178681d1126683cf00cf7fb402dc1a848
Jules Raimbaud <jules.raimbaud@gmail.com>
2024-07-18 10:36:40 +0200

    first router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkbody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+//route pour se signup 
+router.post('/signup', (req, res) => {
+  if (!checkBody(req.body, ['firstname', 'username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  User.findOne({ firstname: req.body.firstname , username: req.body.username }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        firstname: req.body.firstname,
+        username: req.body.username,
+        password: hash,
+        token: uid2(32),
+        canBookmark: true,
+      });
+
+      newUser.save().then(newDoc => {
+        res.json({ result: true, token: newDoc.token });
+      });
+    } else {
+      res.json({ result: false, error: 'User already exists' });
+    }
+  });
 });
+
 
 module.exports = router;
